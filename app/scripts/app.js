5c1227bfca107ca5983366670d1174d7d6f572d4
Neil <neil19962008@gmail.com>
2017-11-04 13:44:56 -0400

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hackAeApp
+ * @description
+ * # hackAeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hackAeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackAeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackAeApp
+ */
+angular.module('hackAeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
