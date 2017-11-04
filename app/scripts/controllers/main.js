ce0c581bd475c376969ec562dc278c2ffe4ceebb
Neil <neil19962008@gmail.com>
2017-11-04 19:57:09 -0400

    Google Autocomplete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,8 @@
  */
 angular.module('hackAeApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	
+	
+
+
   });
